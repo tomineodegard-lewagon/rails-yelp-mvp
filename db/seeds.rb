ffd76b77d131067a41c1b3b1e0883f840f355cc6
Tomine Odegard <tomineodegard@hotmail.no>
2020-08-20 20:46:49 +0200

    Restaurant almost, moving on to the reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  restaurant = Restaurant.create(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.city,
+  category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+end
